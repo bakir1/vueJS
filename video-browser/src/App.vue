d18f2db7748966ba6711c38c7d06461b64cbd86f
bakir1 <37544754+bakir1@users.noreply.github.com>
2021-03-04 17:12:51 +0100

    first part

---
+++ video-browser/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <SearchBar @termChange="onTermChange"></SearchBar>
+        <VideoList></VideoList>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+
+const API_KEY = 'AIzaSyCQZX2duT7acfGqSelUytouerDiv5qXsIM';
+
+export default {
+    name: 'App',
+    components: {
+        SearchBar,
+        VideoList
+    },
+    methods: {
+        onTermChange(searchTerm){
+
+            axios.get('https://www.googleapis.com/youtube/v3/search',{
+                params: {
+                    key: API_KEY,
+                    type: 'video',
+                    part: 'snippet',
+                    q: searchTerm
+                }
+            }).then(response => console.log(response));
+
+        }
+    } 
+};
+</script>
