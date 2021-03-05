ccecebc84cf06e65d3feb9cb40dff6aa5e036d02
bakir1 <37544754+bakir1@users.noreply.github.com>
2021-03-05 14:50:15 +0100

    snippet title

--- video-browser/src/components/VideoListItem.vue
+++ video-browser/src/components/VideoListItem.vue
@@ -1,12 +1,26 @@
 <template>
   <li class="list-group-item">
-      {{ video.snippet.title }}
+    <img class="mr-3" :src="thumbnailUrl" />
+    <div class="media-body">
+       {{ video.snippet.title }}  
+    </div>
   </li>  
 </template>
 
 <script>
 export default {
     name: 'VideoListItem',
-    props: ['video']
+    props: ['video'],
+    computed: {
+      thumbnailUrl(){
+        return this.video.snippet.thumbnails.default.url;
+      }
+    }
 };
 </script>
+
+<style scoped>
+li{
+  display: flex;
+}
+</style>
